053af2d5a9e7468b2ef13293c953fbfddd094ffc
Chen-ZhaoJ <abc205977@gmail.com>
2020-06-01 16:00:18 +0800

    Initial Commit

---
+++ fuck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fuck
+//
+//  Created by Ariel on 2020/6/1.
+//  Copyright © 2020 Ariel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fuckTests/fuckTests.swift
@@ -0,0 +1,34 @@
+//
+//  fuckTests.swift
+//  fuckTests
+//
+//  Created by Ariel on 2020/6/1.
+//  Copyright © 2020 Ariel. All rights reserved.
+//
+
+import XCTest
+@testable import fuck
+
+class fuckTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

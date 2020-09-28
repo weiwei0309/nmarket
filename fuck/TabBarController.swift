dd70eb2b222ee737729c73e806ae2d45536b1e14
Chen-ZhaoJ <abc205977@gmail.com>
2020-09-28 19:08:57 +0800

    guyvng6u6u

--- fuck/TabBarController.swift
+++ fuck/TabBarController.swift
@@ -1,29 +1,21 @@
+//
+//  TabBarController.swift
 //  fuck
 //
-//  Created by Ariel on 2020/6/1.
+//  Created by Rick on 2020/9/28.
 //  Copyright © 2020 Ariel. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController{
-    
-    
-  
+class TabBarController: UITabBarController {
 
-    @IBAction func logout(_ sender: Any) {
-        let userDefaults = UserDefaults.standard
-        _ = userDefaults.value(forKey: "isLogined") as? Bool
-        userDefaults.set(true, forKey: "isLogined")
-        _ = self.storyboard?.instantiateViewController(withIdentifier: "PersonalAccount")
-        // dismiss把畫面砍掉 表示再出現的時候會在viewdidload一次
-        self.dismiss(animated: true, completion: nil)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-    /* override func viewDidAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         let userDefaults = UserDefaults.standard
         let isLogined = userDefaults.value(forKey: "isLogined") as? Bool
         //if isLogined != nil && isLogined! {
@@ -33,7 +25,17 @@ class ViewController: UIViewController{
             let logVC = self.storyboard?.instantiateViewController(withIdentifier: "PersonalAccount")
             present(logVC!, animated: true, completion: nil)
         //}
-    }*/
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
